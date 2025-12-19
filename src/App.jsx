59974ffa65ea93650342be9d17e8e86c147c5ea3
mahimatechbadkur-cyber <mahimatech.badkur@gmail.com>
2025-12-19 15:52:15 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Game from './components/game.jsx'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="app">
+      <h1>Tic Tac Toe</h1>
+      <Game />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Game.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import Board from './Board.jsx'
+
+export default function Game() {
+const [squares, setSquares] = useState(Array(9).fill(null));
+const [xIsNext, setXIsNext] = useState(true);
+
+  function handlePlay(nextSquares) {
+    console.log("value of square", squares);
+    console.log("value of nextSquare", nextSquares);
+     setSquares(nextSquares);
+     setXIsNext(!xIsNext);
+  }
+function resetGame() {
+  setSquares(Array(9).fill(null));
+  setXIsNext(true);
+}
+
+  return (
+    <>
+    <Board xIsNextValue={xIsNext} squares={squares} onPlay={handlePlay} />
+    <button onClick={resetGame}>Reset</button>
+    </>
+  );
+}
